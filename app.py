f2695489f998d17e56d7f7deddfa1f6a4d994a8b
rakshathavasudev <rakshathavasudev@gmail.com>
2025-04-15 19:04:18 -0400

    port change for Railway

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def get_model_response(question):
 #     app.run(debug=False)
 if __name__=="__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
+            port=int(os.getenv('PORT', 8080)))
